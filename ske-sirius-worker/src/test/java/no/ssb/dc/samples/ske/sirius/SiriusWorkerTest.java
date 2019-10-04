b60fb5d483ccbca6f0ab8e5326727806f534e582
oranheim <oranheim@gmail.com>
2019-10-04 12:46:48 +0200

    Implemented Worker.Builder that enables a complete fluent api exec

--- ske-sirius-worker/src/test/java/no/ssb/dc/samples/ske/sirius/SiriusWorkerTest.java
+++ ske-sirius-worker/src/test/java/no/ssb/dc/samples/ske/sirius/SiriusWorkerTest.java
@@ -1,98 +1,90 @@
 package no.ssb.dc.samples.ske.sirius;
 
 import no.ssb.config.DynamicConfiguration;
-import no.ssb.dc.api.ConfigurationMap;
+import no.ssb.config.StoreBasedDynamicConfiguration;
+import no.ssb.dc.api.Builders;
 import no.ssb.dc.api.Flow;
-import no.ssb.dc.api.content.ContentStore;
-import no.ssb.dc.api.content.ContentStoreInitializer;
 import no.ssb.dc.api.context.ExecutionContext;
-import no.ssb.dc.api.http.Client;
-import no.ssb.dc.api.services.Services;
 import no.ssb.dc.api.util.CommonUtils;
-import no.ssb.dc.core.executor.BufferedReordering;
-import no.ssb.dc.core.executor.FixedThreadPool;
 import no.ssb.dc.core.executor.Worker;
-import no.ssb.dc.core.handler.ParallelHandler;
-import no.ssb.dc.core.security.CertificateFactory;
-import no.ssb.dc.test.server.TestServer;
-import no.ssb.dc.test.server.TestServerListener;
-import no.ssb.service.provider.api.ProviderConfigurator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Ignore;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import javax.inject.Inject;
-import javax.net.ssl.SSLContext;
+import java.util.concurrent.CompletableFuture;
 
-import static org.testng.Assert.assertNotNull;
+import static no.ssb.dc.api.Builders.addContent;
+import static no.ssb.dc.api.Builders.context;
+import static no.ssb.dc.api.Builders.eval;
+import static no.ssb.dc.api.Builders.execute;
+import static no.ssb.dc.api.Builders.get;
+import static no.ssb.dc.api.Builders.nextPage;
+import static no.ssb.dc.api.Builders.paginate;
+import static no.ssb.dc.api.Builders.parallel;
+import static no.ssb.dc.api.Builders.publish;
+import static no.ssb.dc.api.Builders.sequence;
+import static no.ssb.dc.api.Builders.status;
+import static no.ssb.dc.api.Builders.whenVariableIsNull;
+import static no.ssb.dc.api.Builders.xpath;
 
-@Listeners(TestServerListener.class)
 public class SiriusWorkerTest {
-
-    private static final Logger LOG = LoggerFactory.getLogger(SiriusWorkerTest.class);
-
-    @Inject
-    TestServer testServer;
-
-    Services services;
-
-    DynamicConfiguration configuration;
-
-    ContentStore contentStore;
-
-    @BeforeMethod
-    public void setup() {
-        Client.Builder clientBuilder = Client.newClientBuilder();
-
-        CertificateFactory sslFactory = CertificateFactory.scanAndCreate(CommonUtils.currentPath());
-        SSLContext sslContext = sslFactory.getSSLContext("certs");;
-        assertNotNull(sslContext);
-
-        clientBuilder.sslContext(sslContext);
-
-        configuration = testServer.getConfiguration();
-        contentStore = ProviderConfigurator.configure(configuration.asMap(),
-                configuration.evaluateToString("content.store.provider"), ContentStoreInitializer.class);
-        services = Services.create()
-                .register(ConfigurationMap.class, new ConfigurationMap(configuration.asMap()))
-                .register(Client.class, clientBuilder.build())
-                .register(BufferedReordering.class, new BufferedReordering<>())
-                .register(ContentStore.class, contentStore)
-                .register(FixedThreadPool.class, FixedThreadPool.newInstance(configuration));
-    }
-
-    @Test
-    public void testPrintConfig() {
-        LOG.info("Config:\n{}", SiriusFlow.builder().serialize());
-//        LOG.info("Config:\n{}", SiriusFlow.getFlow().end().startNode());
-    }
 
     @Ignore
     @Test
     public void thatWorkerCollectSiriusFlow() throws InterruptedException {
-        ExecutionContext context = new ExecutionContext.Builder().services(services).build();
+        DynamicConfiguration configuration = new StoreBasedDynamicConfiguration.Builder().values("content.store.provider", "discarding").build();
 
-        context.state(ParallelHandler.MAX_NUMBER_OF_ITERATIONS, 1);
+        CompletableFuture<ExecutionContext> future = Worker.newBuilder()
+                .flow(Flow.start("Collect Sirius", "loop")
+                        .configure(context()
+                                        .topic("sirius")
+                                        .header("accept", "application/xml")
+                                        .variable("baseURL", "https://api-at.sits.no")
+                                //.variable("fromSequence", "${contentStore.lastPosition(topic) == null ? initialPosition : contentStore.lastPosition(topic)}")
+                        )
+                        .node(paginate("loop")
+                                .variable("fromSequence", "${nextSequence}")
+                                .addPageContent()
+                                .step(execute("part"))
+                                .prefetchThreshold(0.5)
+                                .until(whenVariableIsNull("nextSequence"))
+                        )
+                        .node(get("part")
+                                .url("${baseURL}/api/formueinntekt/skattemelding/utkast/hendelser/?fraSekvensnummer=${fromSequence}&antall=100")
+                                .validate(status().success(200).fail(400).fail(404).fail(500))
+                                .step(sequence(xpath("/hendelser/hendelse"))
+                                        .expected(xpath("/hendelse/sekvensnummer"))
+                                )
+                                .step(nextPage()
+                                        .output("nextSequence",
+                                                eval(xpath("/hendelser/hendelse[last()]/sekvensnummer"), "lastSequence", "${cast.toLong(lastSequence) + 1}")
+                                        )
+                                )
+                                .step(parallel(xpath("/hendelser/hendelse"))
+                                        .variable("position", xpath("/hendelse/sekvensnummer"))
+                                        .step(addContent("${position}", "entry"))
+                                        .step(execute("utkast-melding")
+                                                .inputVariable("utkastIdentifikator", xpath("/hendelse/identifikator"))
+                                                .inputVariable("year", xpath("/hendelse/gjelderPeriode"))
+                                        )
+                                        .step(publish("${position}"))
+                                )
+                                .returnVariables("nextSequence")
+                        )
+                        .node(Builders.get("utkast-melding")
+                                .url("${baseURL}/api/formueinntekt/skattemelding/utkast/ssb/${year}/${utkastIdentifikator}")
+                                .step(addContent("${position}", "utkastIdentifikator"))
+                        ))
+                .configurationMap(configuration.asMap())
+                .sslContext(CommonUtils.currentPath(), "certs")
+                .initialPosition("1")
+                .initialPositionVariable("fromSequence")
+                .maxNumberOfParallelIterations(1)
+                .printExecutionPlan()
+                .printConfiguration()
+                .build()
+                .run();
 
-        String lastPosition = contentStore.lastPosition(configuration.evaluateToString("namespace.default"));
-        String startPosition = (lastPosition == null ? "1" : lastPosition);
-        context.variable("fromSequence", startPosition);
+        future.join();
 
-        Flow flow = SiriusFlow.builder().end();
-
-        Worker worker = new Worker(flow.startNode(), context);
-        worker.run();
-
-//        Worker.newBuilder()
-//                .flow(SiriusFlow.get())
-//                .services(services)
-//                .initialPosition("1")
-//                .header("Accept", "application/xml")
-//                .variable("baseURL", "https://api-at.sits.no")
-//                .build()
-//                .run();
     }
 }

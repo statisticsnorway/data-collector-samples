3071f77f4c58e810979b40431a726c94b6fa92ec
oranheim <oranheim@gmail.com>
2019-10-07 13:06:16 +0200

    Renamed DSL: node to function and step to pipe

--- ske-sirius-worker/src/test/java/no/ssb/dc/samples/ske/sirius/SiriusWorkerTest.java
+++ ske-sirius-worker/src/test/java/no/ssb/dc/samples/ske/sirius/SiriusWorkerTest.java
@@ -3,7 +3,6 @@ package no.ssb.dc.samples.ske.sirius;
 import no.ssb.dc.api.Flow;
 import no.ssb.dc.api.util.CommonUtils;
 import no.ssb.dc.core.executor.Worker;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import java.util.Map;
@@ -26,7 +25,7 @@ import static no.ssb.dc.api.Builders.xpath;
 // https://skatteetaten.github.io/datasamarbeid-api-dokumentasjon/reference_skattemelding
 public class SiriusWorkerTest {
 
-    @Ignore
+//    @Ignore
     @Test
     public void thatWorkerCollectSiriusData() throws InterruptedException {
         Worker.newBuilder()
@@ -54,39 +53,39 @@ public class SiriusWorkerTest {
                         .configure(security()
                                 .sslBundleName("ske-test-certs")
                         )
-                        .node(paginate("loop")
+                        .function(paginate("loop")
                                 .variable("fromSequence", "${nextSequence}")
                                 .addPageContent()
-                                .step(execute("parts"))
+                                .iterate(execute("parts"))
                                 .prefetchThreshold(0.5)
                                 .until(whenVariableIsNull("nextSequence"))
                         )
-                        .node(get("parts")
+                        .function(get("parts")
                                 .url("${baseURL}/api/formueinntekt/skattemelding/utkast/hendelser/?fraSekvensnummer=${fromSequence}&antall=${hentAntallMeldingerOmGangen}")
                                 .validate(status().success(200).fail(400).fail(404).fail(500))
-                                .step(sequence(xpath("/hendelser/hendelse"))
+                                .pipe(sequence(xpath("/hendelser/hendelse"))
                                         .expected(xpath("/hendelse/sekvensnummer"))
                                 )
-                                .step(nextPage()
+                                .pipe(nextPage()
                                         .output("nextSequence",
                                                 eval(xpath("/hendelser/hendelse[last()]/sekvensnummer"), "lastSequence", "${cast.toLong(lastSequence) + 1}")
                                         )
                                 )
-                                .step(parallel(xpath("/hendelser/hendelse"))
+                                .pipe(parallel(xpath("/hendelser/hendelse"))
                                         .variable("position", xpath("/hendelse/sekvensnummer"))
-                                        .step(addContent("${position}", "entry"))
-                                        .step(execute("utkast-melding")
+                                        .pipe(addContent("${position}", "entry"))
+                                        .pipe(execute("utkast-melding")
                                                 .inputVariable("utkastIdentifikator", xpath("/hendelse/identifikator"))
                                                 .inputVariable("year", xpath("/hendelse/gjelderPeriode"))
                                         )
-                                        .step(publish("${position}"))
+                                        .pipe(publish("${position}"))
                                 )
                                 .returnVariables("nextSequence")
                         )
-                        .node(get("utkast-melding")
+                        .function(get("utkast-melding")
                                 .url("${baseURL}/api/formueinntekt/skattemelding/${hendelse}/${rettighetspakke}/${year}/${utkastIdentifikator}")
                                 .validate(status().success(200).fail(400).fail(404).fail(500))
-                                .step(addContent("${position}", "utkastSkatteMelding"))
+                                .pipe(addContent("${position}", "skattemelding"))
                         ))
                 .build()
                 .run();

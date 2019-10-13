f7a9ff5794ca8988342661a7a85b4c903a9abdb7
oranheim <oranheim@gmail.com>
2019-10-13 20:12:54 +0200

    Added Toll Tvinn worker test

---
+++ toll-tvinn-worker/src/test/java/no/ssb/dc/samples/toll/tvinn/TvinnWorkerTest.java
@@ -0,0 +1,88 @@
+package no.ssb.dc.samples.toll.tvinn;
+
+import no.ssb.config.StoreBasedDynamicConfiguration;
+import no.ssb.dc.api.Specification;
+import no.ssb.dc.api.util.CommonUtils;
+import no.ssb.dc.core.executor.Worker;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+
+import static no.ssb.dc.api.Builders.addContent;
+import static no.ssb.dc.api.Builders.context;
+import static no.ssb.dc.api.Builders.execute;
+import static no.ssb.dc.api.Builders.get;
+import static no.ssb.dc.api.Builders.nextPage;
+import static no.ssb.dc.api.Builders.paginate;
+import static no.ssb.dc.api.Builders.parallel;
+import static no.ssb.dc.api.Builders.publish;
+import static no.ssb.dc.api.Builders.regex;
+import static no.ssb.dc.api.Builders.security;
+import static no.ssb.dc.api.Builders.sequence;
+import static no.ssb.dc.api.Builders.status;
+import static no.ssb.dc.api.Builders.whenVariableIsNull;
+import static no.ssb.dc.api.Builders.xpath;
+
+public class TvinnWorkerTest {
+
+    @Ignore
+    @Test
+    public void testCollectTvinn() {
+        Worker.newBuilder()
+                .configuration(new StoreBasedDynamicConfiguration.Builder()
+                        .values("content.store.provider", "discarding")
+                        .values("rawdata.client.provider", "memory")
+                        .values("data.collector.worker.threads", "25")
+                        .values("postgres.driver.host", "localhost")
+                        .values("postgres.driver.port", "5432")
+                        .values("postgres.driver.user", "rdc")
+                        .values("postgres.driver.password", "rdc")
+                        .values("postgres.driver.database", "rdc")
+                        .values("postgres.recreate-database", "false")
+                        .values("rawdata.postgres.consumer.prefetch-size", "100")
+                        .values("rawdata.postgres.consumer.prefetch-poll-interval-when-empty", "1000")
+                        .build()
+                        .asMap())
+                .buildCertificateFactory(CommonUtils.currentPath())
+                //.stopAtNumberOfIterations(5)
+                .printConfiguration()
+                .specification(Specification.start("Collect Tvinn", "loop")
+                        .configure(context()
+                                .topic("tvinn")
+                                .header("accept", "application/xml")
+                                .variable("TestURL", "https://mp-at.toll.no")
+                                .variable("ProduksjonURL", "https://mp-sit.toll.no")
+                                .variable("fromMarker", "last")
+                        )
+                        .configure(security()
+                                .sslBundleName("toll-test-certs")
+                        )
+                        .function(paginate("loop")
+                                .variable("fromMarker", "${nextMarker}")
+                                .addPageContent()
+                                .iterate(execute("event-list"))
+                                .prefetchThreshold(150)
+                                .until(whenVariableIsNull("nextMarker"))
+                        )
+                        .function(get("event-list")
+                                .url("${TestURL}/atomfeed/toll/deklarasjon-ekstern-feed/?marker=${fromMarker}&limit=25&direction=forward")
+                                .validate(status().success(200))
+                                .pipe(sequence(xpath("/feed/entry"))
+                                        .expected(xpath("/entry/id"))
+                                )
+                                .pipe(nextPage()
+                                        .output("nextMarker",
+                                                regex(xpath("/feed/link[@rel=\"previous\"]/@href"), "(?<=[?&]marker=)[^&]*")
+                                        )
+                                )
+                                .pipe(parallel(xpath("/feed/entry"))
+                                        .variable("position", xpath("/entry/id"))
+                                        .pipe(addContent("${position}", "entry"))
+                                        .pipe(publish("${position}"))
+                                )
+                                .returnVariables("nextMarker")
+                        )
+                )
+                .build()
+                .run();
+    }
+}

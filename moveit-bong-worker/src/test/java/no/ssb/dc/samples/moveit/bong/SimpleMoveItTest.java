53230eda6ed51c2c028a991d4bb262837dc0d7d4
oranheim <oranheim@gmail.com>
2020-05-26 17:31:45 +0200

    Upload file using MultipartBodyPublisher

--- moveit-bong-worker/src/test/java/no/ssb/dc/samples/moveit/bong/SimpleMoveItTest.java
+++ moveit-bong-worker/src/test/java/no/ssb/dc/samples/moveit/bong/SimpleMoveItTest.java
@@ -2,6 +2,7 @@ package no.ssb.dc.samples.moveit.bong;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.github.mizosoft.methanol.MultipartBodyPublisher;
 import no.ssb.config.DynamicConfiguration;
 import no.ssb.config.StoreBasedDynamicConfiguration;
 import no.ssb.dc.api.Builders;
@@ -16,6 +17,10 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.net.http.HttpRequest;
+import java.nio.ByteBuffer;
+import java.util.concurrent.Flow;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -56,6 +61,42 @@ public class SimpleMoveItTest {
         return response;
     }
 
+    static Response POST(String url, String accessToken, byte[] payload) {
+        return POST(url, accessToken, payload, null);
+    }
+
+    static Response POST(String url, String accessToken, byte[] payload, String contentType) {
+        Request.Builder requestBuilder = Request.newRequestBuilder()
+                .url("https://moveitapitest.ssb.no" + url)
+                .header("Authorization", "Bearer " + accessToken);
+        if (contentType != null) {
+            requestBuilder.header("Content-Type", contentType);
+        }
+        Request request = requestBuilder.POST(payload).build();
+
+        Response response = client.send(request);
+        if (!(response.statusCode() == 200 || response.statusCode() == 201)) {
+            throw new RuntimeException(String.format("Response error: [%s]: %s", response.statusCode(), new String(response.body())));
+        }
+        return response;
+    }
+
+    static Response POST(String url, String accessToken, Flow.Publisher<ByteBuffer> bodyPublisher, String boundary) {
+        Request.Builder requestBuilder = Request.newRequestBuilder()
+                .url("https://moveitapitest.ssb.no" + url)
+                .header("Authorization", "Bearer " + accessToken);
+        requestBuilder
+                .header("Content-Type", "multipart/form-data;boundary=" + boundary);
+        requestBuilder
+                .POST(bodyPublisher);
+
+        Response response = client.send(requestBuilder.build());
+        if (!(response.statusCode() == 200 || response.statusCode() == 201)) {
+            throw new RuntimeException(String.format("Response error: [%s]: %s", response.statusCode(), new String(response.body())));
+        }
+        return response;
+    }
+
     static String jq(byte[] data, String expression) {
         JqPath jqPathAccessToken = Builders.jqpath(expression).build();
         QueryFeature jqAccessToken = Queries.from(jqPathAccessToken);
@@ -83,14 +124,11 @@ public class SimpleMoveItTest {
 
     @Disabled
     @Test
-    void flow() {
+    void downloadFlow() {
         Response responseToken = POST("/api/v1/token", AUTHORIZATION_DATA);
         String accessToken = jq(responseToken.body(), ".access_token");
 
-        // --
-
         Response responseListFiles = GET("/api/v1/files", accessToken);
-
         JsonNode jsonNode = fromJson(responseListFiles.body());
         //LOG.trace("{}", toPrettyJSON(jsonNode));
 
@@ -103,8 +141,6 @@ public class SimpleMoveItTest {
             String fileUploadStamp = jq(itemNode, ".uploadStamp");
             //LOG.trace("id: {}", fileId);
 
-            // get file info
-
             Response responseFileInfo = GET("/api/v1/files/" + fileId, accessToken);
             JsonNode fileNode = fromJson(responseFileInfo.body());
             //LOG.trace("{}", toPrettyJSON(fileNode));
@@ -115,4 +151,24 @@ public class SimpleMoveItTest {
             LOG.trace("download: {} -> {}", fileNamePath, new String(download.body()));
         }
     }
+
+    @Disabled
+    @Test
+    void uploadFlow() {
+        Response responseToken = POST("/api/v1/token", AUTHORIZATION_DATA);
+        String accessToken = jq(responseToken.body(), ".access_token");
+
+        Response responseListFolders = GET("/api/v1/folders", accessToken);
+        JsonNode jsonNode = fromJson(responseListFolders.body());
+        String folderId = jq(jsonNode, ".items[] | select(.path == \"/Home/moveitapi\") | .id");
+        assertEquals("672079599", folderId);
+
+        MultipartBodyPublisher multipartBody = MultipartBodyPublisher.newBuilder()
+                .formPart("file", "file13.txt", HttpRequest.BodyPublishers.ofByteArray("hello again".getBytes()))
+                .build();
+
+        Response responseUpload = POST(String.format("/api/v1/folders/%s/files", "672079599"), accessToken, multipartBody, multipartBody.boundary());
+        LOG.trace("responseUpload: {}", new String(responseUpload.body()));
+    }
+
 }
